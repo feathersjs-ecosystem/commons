5f89135445be90251d02fcb50635b5aee1062652
manu <emmanuel.bourmalo@gmail.com>
2015-07-09 09:22:38 +0200

    extract find and create converters into methods

--- src/arguments.js
+++ src/arguments.js
@@ -3,6 +3,33 @@ import _ from 'lodash';
 const getCallback = args => typeof _.last(args) === 'function' ? _.last(args) : _.noop;
 const getParams = (args, position) => typeof args[position] === 'object' ? args[position] : {};
 
+const find = args => {
+  let callback = getCallback(args);
+  let params = getParams(args, 0);
+
+  if(args.length > 2) {
+    throw new Error(`Too many arguments for 'find' service method`);
+  }
+
+  return [ params, callback ];
+};
+
+const create = args => {
+  let data = args[0];
+  let params = getParams(args, 1);
+  let callback = getCallback(args);
+
+  if(typeof data !== 'object') {
+    throw new Error(`First parameter for 'create' must be an object`);
+  }
+
+  if(args.length > 3) {
+    throw new Error(`Too many arguments for 'create' service method`);
+  }
+
+  return [ data, params, callback ];
+};
+
 const updateOrPatch = name => {
   return function(args) {
     let id = args[0];
@@ -44,33 +71,15 @@ const getOrRemove = name => {
   };
 };
 
+
+
+
+
+
 export const converters = {
-  find(args) {
-    let callback = getCallback(args);
-    let params = getParams(args, 0);
+  find: find,
 
-    if(args.length > 2) {
-      throw new Error(`Too many arguments for 'find' service method`);
-    }
-
-    return [ params, callback ];
-  },
-
-  create(args) {
-    let data = args[0];
-    let params = getParams(args, 1);
-    let callback = getCallback(args);
-
-    if(typeof data !== 'object') {
-      throw new Error(`First parameter for 'create' must be an object`);
-    }
-
-    if(args.length > 3) {
-      throw new Error(`Too many arguments for 'create' service method`);
-    }
-
-    return [ data, params, callback ];
-  },
+  create: create,
 
   update: updateOrPatch('update'),
 
@@ -78,7 +87,7 @@ export const converters = {
 
   get: getOrRemove('get'),
 
-  remove: getOrRemove('remove')
+  remove: getOrRemove('remove'),
 };
 
 export default function getArguments(method, args) {
