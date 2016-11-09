52aba5cd5907b2d07c8acaf1e6b2d5ccce0d4082
David Luecke <daff@neyeon.com>
2016-11-08 21:17:31 -0800

    Implementing lodash utilities and helpers for selecting

--- src/commons.js
+++ src/commons.js
@@ -1,8 +1,9 @@
 import getArguments from './arguments';
-import { stripSlashes, each, matcher, sorter } from './utils';
+import { stripSlashes, each, matcher, sorter, _ } from './utils';
 import hooks from './hooks';
 
 export default {
+  _,
   getArguments,
   stripSlashes,
   each,
